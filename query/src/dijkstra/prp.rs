483d2a3fb85ec7daf021f5995efe2047e7324fec
Felix Buehler <account@buehler.rocks>
2021-06-06 13:01:18 +0200

    [query] prp: first implementation

--- query/src/dijkstra/prp.rs
+++ query/src/dijkstra/prp.rs
@@ -7,16 +7,16 @@ use valid_flag::*;
 use mch::costs_by_alpha;
 use std::collections::BinaryHeap;
 
-// TODO use 2 heaps for pch and crp
-
 #[derive(Clone)]
 pub struct Dijkstra<E: Export> {
     dist_up: Vec<(Cost, Option<NodeId>)>,
     dist_down: Vec<(Cost, Option<NodeId>)>,
     visited_up: ValidFlag,
     visited_down: ValidFlag,
-    heap_up: BinaryHeap<MinHeapItem>,
-    heap_down: BinaryHeap<MinHeapItem>,
+    heap_pch_up: BinaryHeap<MinHeapItem>,
+    heap_pch_down: BinaryHeap<MinHeapItem>,
+    heap_crp_up: BinaryHeap<MinHeapItem>,
+    heap_crp_down: BinaryHeap<MinHeapItem>,
     pub exporter: E,
 }
 
@@ -27,15 +27,19 @@ impl<E: Export> FindPath<E> for Dijkstra<E> {
         let dist_down = vec![(COST_MAX, None); amount_nodes];
         let visited_up = ValidFlag::new(amount_nodes);
         let visited_down = ValidFlag::new(amount_nodes);
-        let heap_up = BinaryHeap::with_capacity(amount_nodes);
-        let heap_down = BinaryHeap::with_capacity(amount_nodes);
+        let heap_pch_up = BinaryHeap::with_capacity(amount_nodes);
+        let heap_pch_down = BinaryHeap::with_capacity(amount_nodes);
+        let heap_crp_up = BinaryHeap::with_capacity(amount_nodes);
+        let heap_crp_down = BinaryHeap::with_capacity(amount_nodes);
         Dijkstra {
             dist_up,
             dist_down,
             visited_up,
             visited_down,
-            heap_up,
-            heap_down,
+            heap_pch_up,
+            heap_pch_down,
+            heap_crp_up,
+            heap_crp_down,
             exporter,
         }
     }
@@ -44,8 +48,10 @@ impl<E: Export> FindPath<E> for Dijkstra<E> {
     fn reset_state(&mut self) {
         self.visited_up.invalidate_all();
         self.visited_down.invalidate_all();
-        self.heap_up.clear();
-        self.heap_down.clear();
+        self.heap_pch_up.clear();
+        self.heap_pch_down.clear();
+        self.heap_crp_up.clear();
+        self.heap_crp_down.clear();
         self.exporter.reset();
     }
 
@@ -70,22 +76,11 @@ impl<E: Export> FindPath<E> for Dijkstra<E> {
         self.visited_down.set_valid(to);
         self.dist_down[to] = (0.0, None);
 
-        self.heap_up.push(MinHeapItem::new(from, 0.0, None));
-        self.heap_down.push(MinHeapItem::new(to, 0.0, None));
+        self.heap_pch_up.push(MinHeapItem::new(from, 0.0, None));
+        self.heap_pch_down.push(MinHeapItem::new(to, 0.0, None));
 
         let mut best_cost = COST_MAX;
         let mut meeting_node = None;
-
-        // get maximum crp-layer partition
-        let highes_diff_layer =
-            mlp_helper::get_highest_differing_level(from, to, &nodes, &mlp_layers);
-        let common_partition =
-            mlp_helper::get_partition_id_on_level(from, highes_diff_layer, &nodes, &mlp_layers);
-        // println!(
-        //     "highes_diff_layer {:?} common_partition {:?}",
-        //     highes_diff_layer, common_partition
-        // );
-        // TODO not working!!!
 
         // function pointers for only having one single dijkstra
         let get_up_edge_ids: fn(&Graph, NodeId) -> Vec<EdgeId> = Graph::get_up_edge_ids;
@@ -93,13 +88,14 @@ impl<E: Export> FindPath<E> for Dijkstra<E> {
         let get_to: fn(&Edge) -> NodeId = Edge::get_to;
         let get_from: fn(&Edge) -> NodeId = Edge::get_from;
 
-        while let Some((
+        'outer: while let Some((
             MinHeapItem {
                 node,
                 cost,
-                prev_edge,
+                prev_edge: _,
             },
             heap,
+            crp_heap,
             visited,
             dist,
             walk,
@@ -108,27 +104,28 @@ impl<E: Export> FindPath<E> for Dijkstra<E> {
             dist_,
             exporter,
         )) = {
-            if self.heap_up.is_empty() && self.heap_down.is_empty() {
-                return None;
+            if self.heap_pch_up.is_empty() && self.heap_pch_down.is_empty() {
+                break 'outer;
             }
             let next_up: Cost = self
-                .heap_up
+                .heap_pch_up
                 .peek()
                 .unwrap_or(&MinHeapItem::new(INVALID_NODE, COST_MAX, None))
                 .cost;
             let next_down: Cost = self
-                .heap_down
+                .heap_pch_down
                 .peek()
                 .unwrap_or(&MinHeapItem::new(INVALID_NODE, COST_MAX, None))
                 .cost;
-            // TODO maybe also look what node is on lower layer and pick that one
-            if next_up + next_down >= best_cost {
+            // if no improvement can be found by one of the edges
+            if next_up >= best_cost && next_down >= best_cost {
                 None
             } else if next_up <= next_down {
-                self.heap_up.pop().map(|x| {
+                self.heap_pch_up.pop().map(|x| {
                     (
                         x,
-                        &mut self.heap_up,
+                        &mut self.heap_pch_up,
+                        &mut self.heap_crp_up,
                         &mut self.visited_up,
                         &mut self.dist_up,
                         get_to,
@@ -139,10 +136,11 @@ impl<E: Export> FindPath<E> for Dijkstra<E> {
                     )
                 })
             } else {
-                self.heap_down.pop().map(|x| {
+                self.heap_pch_down.pop().map(|x| {
                     (
                         x,
-                        &mut self.heap_down,
+                        &mut self.heap_pch_down,
+                        &mut self.heap_crp_down,
                         &mut self.visited_down,
                         &mut self.dist_down,
                         get_from,
@@ -161,63 +159,156 @@ impl<E: Export> FindPath<E> for Dijkstra<E> {
                 continue;
             }
 
-            visited.set_valid(node);
-            dist[node] = (cost, prev_edge);
-
-            exporter.visited_node(node);
-            exporter.visited_edge(prev_edge);
-
             for edge in get_edges(&graph, node) {
                 let next = walk(&graph.get_edge(edge));
 
-                exporter.relaxed_edge();
-
-                // in lowest layer
-                if nodes[node].layer_height == 0 {
-                    // skip pch ranks
-                    // top-layer nodes have maximum layer number so no equal test
-                    if nodes[node].rank > nodes[next].rank {
-                        break;
-                    }
-                // in top layer
-                } else {
-                    // walk only in layers above
-                    if nodes[node].layer_height > nodes[next].layer_height {
-                        break;
-                    }
-                    // do not walk in partitons, that are excluded
-                    if mlp_helper::get_partition_id_on_level(
-                        next,
-                        highes_diff_layer,
-                        &nodes,
-                        &mlp_layers,
-                    ) != common_partition
-                    {
-                        continue;
-                    }
-                    // do not walk in partitons higher then the common one
-                    if graph.get_edge(edge).layer > highes_diff_layer {
-                        continue;
-                    }
+                // skip pch ranks
+                // top-layer nodes have maximum layer number so no equal test
+                if nodes[node].rank > nodes[next].rank {
+                    break;
                 }
+
+                exporter.relaxed_edge();
 
                 let alt = cost + costs_by_alpha(&graph.get_edge_costs(edge), &alpha);
 
                 if !visited.is_valid(next) || alt < dist[next].0 {
-                    heap.push(MinHeapItem::new(next, alt, Some(edge)));
+                    if nodes[next].partition != nodes[next].partition {
+                        // if node is in other partition insert it into crp_heap
+                        crp_heap.push(MinHeapItem::new(next, alt, Some(edge)));
+                    } else {
+                        // otherwise
+                        heap.push(MinHeapItem::new(next, alt, Some(edge)));
+                    }
+                    visited.set_valid(next);
+                    dist[next] = (alt, Some(edge));
 
+                    exporter.visited_node(next);
+                    exporter.visited_edge(Some(edge));
                     // check if other dijkstra has visited this point before
-                    if visited_.is_valid(node) {
-                        let combined = dist_[node].0 + alt;
+                    if visited_.is_valid(next) {
+                        let combined = dist_[next].0 + alt;
                         if combined < best_cost {
-                            meeting_node = Some(node);
-                            exporter.current_meeting_point(node);
+                            meeting_node = Some(next);
+                            exporter.current_meeting_point(next);
                             best_cost = combined;
                         }
                     }
                 }
             }
         }
+
+        while let Some((
+            MinHeapItem {
+                node,
+                cost,
+                prev_edge: _,
+            },
+            heap,
+            visited,
+            dist,
+            walk,
+            get_edges,
+            visited_,
+            dist_,
+            exporter,
+        )) = {
+            let next_up: Cost = self
+                .heap_crp_up
+                .peek()
+                .map(|min_item| min_item.cost)
+                .unwrap_or(COST_MAX);
+            let next_down: Cost = self
+                .heap_crp_down
+                .peek()
+                .map(|min_item| min_item.cost)
+                .unwrap_or(COST_MAX);
+            if next_up + next_down > best_cost {
+                None
+            } else if next_up <= next_down {
+                self.heap_crp_up.pop().map(|x| {
+                    (
+                        x,
+                        &mut self.heap_crp_up,
+                        &mut self.visited_up,
+                        &mut self.dist_up,
+                        get_to,
+                        get_up_edge_ids,
+                        &mut self.visited_down,
+                        &mut self.dist_down,
+                        &mut self.exporter,
+                    )
+                })
+            } else {
+                self.heap_crp_down.pop().map(|x| {
+                    (
+                        x,
+                        &mut self.heap_crp_down,
+                        &mut self.visited_down,
+                        &mut self.dist_down,
+                        get_from,
+                        get_down_edge_ids,
+                        &mut self.visited_up,
+                        &mut self.dist_up,
+                        &mut self.exporter,
+                    )
+                })
+            }
+        } {
+            exporter.heap_pop();
+            // node has already been visited and can be skipped
+            // replacement for decrease key operation
+            if visited.is_valid(node) && cost > dist[node].0 {
+                continue;
+            }
+
+            // get query level on which to walk
+            let query_layer = std::cmp::min(
+                mlp_helper::get_highest_differing_level(from, node, &nodes, &mlp_layers),
+                mlp_helper::get_highest_differing_level(node, to, &nodes, &mlp_layers),
+            );
+
+            for edge_id in get_edges(&graph, node) {
+                let edge = graph.get_edge(edge_id);
+
+                // skip edges, that are shortcuts-resultions from upper layers
+                if edge.layer.is_none() {
+                    continue;
+                }
+                // only walk on query layers and never below
+                if query_layer > edge.layer.unwrap() {
+                    continue;
+                }
+
+                // if query_layer <= edge.layer.unwrap() && edge.contrated_edges.is_some() {
+                //     continue;
+                // }
+
+                let next = walk(&edge);
+                exporter.relaxed_edge();
+
+                let alt = cost + costs_by_alpha(&graph.get_edge_costs(edge_id), &alpha);
+
+                if !visited.is_valid(next) || alt < dist[next].0 {
+                    heap.push(MinHeapItem::new(next, alt, Some(edge_id)));
+                    visited.set_valid(next);
+                    dist[next] = (alt, Some(edge_id));
+
+                    exporter.visited_node(next);
+                    exporter.visited_edge(Some(edge_id));
+                    // check if other dijkstra has visited this point before
+                    if visited_.is_valid(next) {
+                        let combined = dist_[next].0 + alt;
+                        if combined < best_cost {
+                            meeting_node = Some(next);
+                            exporter.current_meeting_point(next);
+                            best_cost = combined;
+                        }
+                    }
+                }
+            }
+        }
+
         match meeting_node {
             Some(meet_node) => {
                 Some(self.resolve_path(meet_node, best_cost, nodes[meet_node].rank, &graph.edges))
@@ -245,8 +336,8 @@ impl<E: Export> Dijkstra<E> {
 
         if let Some(prev_edge) = up_edge.1 {
             self.walk_down(prev_edge, true, &mut path, &edges);
-            path.reverse();
         }
+        path.reverse();
         if let Some(prev_edge) = down_edge.1 {
             self.walk_down(prev_edge, false, &mut path, &edges);
         }
@@ -264,15 +355,12 @@ impl<E: Export> Dijkstra<E> {
         self.resolve_edge(edge, &mut path, is_upwards, &edges);
 
         let current_edge = &edges[edge];
-        let next;
         let prev;
 
         if is_upwards {
-            next = current_edge.from;
-            prev = self.dist_up[next];
+            prev = self.dist_up[current_edge.from];
         } else {
-            next = current_edge.to;
-            prev = self.dist_down[next];
+            prev = self.dist_down[current_edge.to];
         }
         if let Some(child) = prev.1 {
             self.walk_down(child, is_upwards, &mut path, &edges);
@@ -287,26 +375,17 @@ impl<E: Export> Dijkstra<E> {
         is_upwards: bool,
         edges: &[Edge],
     ) {
-        let current_edge = &edges[edge];
-
-        if is_upwards {
-            if let Some(next) = current_edge.contrated_edges {
-                self.resolve_edge(next.1, &mut path, is_upwards, &edges);
+        match &edges[edge].contrated_edges {
+            Some(shortcut) => {
+                if is_upwards {
+                    self.resolve_edge(shortcut.1, &mut path, is_upwards, &edges);
+                    self.resolve_edge(shortcut.0, &mut path, is_upwards, &edges);
+                } else {
+                    self.resolve_edge(shortcut.0, &mut path, is_upwards, &edges);
+                    self.resolve_edge(shortcut.1, &mut path, is_upwards, &edges);
+                }
             }
-            if let Some(previous) = current_edge.contrated_edges {
-                self.resolve_edge(previous.0, &mut path, is_upwards, &edges);
-            } else {
-                path.push(edge);
-            }
-        } else {
-            if let Some(previous) = current_edge.contrated_edges {
-                self.resolve_edge(previous.0, &mut path, is_upwards, &edges);
-            }
-            if let Some(next) = current_edge.contrated_edges {
-                self.resolve_edge(next.1, &mut path, is_upwards, &edges);
-            } else {
-                path.push(edge);
-            }
+            None => path.push(edge),
         }
     }
 }
