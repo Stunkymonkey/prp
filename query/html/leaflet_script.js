610dd67b4e345ddb570dd9c102cbe36312fd41ab
Felix Buehler <account@buehler.rocks>
2021-06-13 13:56:19 +0200

    [html] replace var with let/const

--- query/html/leaflet_script.js
+++ query/html/leaflet_script.js
@@ -1,4 +1,4 @@
-var map = L.map('map', {
+let map = L.map('map', {
     maxBounds: [
         [47.1, 5.7], // Southwest coordinates
         [55.2, 16.9] // Northeast coordinates
@@ -18,9 +18,10 @@ let startMarker;
 let endPoint;
 let endMarker;
 let tmpMarker;
-var last_path;
+let last_path;
 let xhr = new XMLHttpRequest();
-var metrics;
+let metrics;
+
 getMetrics();
 
 function onMapClick(e) {
@@ -33,7 +34,7 @@ function onMapClick(e) {
 }
 
 function getMetrics() {
-    var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open("GET", window.location.href + "metrics", true);
     xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
 
@@ -50,10 +51,13 @@ function getMetrics() {
 }
 
 function setSlider(metrics) {
-    var slide_container = "";
+    let slide_container = "";
     metrics.forEach(function (item, index) {
         // console.log(item, index);
-        slide_container += item + "<input type=\"range\" min=\"0\" max=\"1\" value=\"0.25\" class=\"slider\" step=\"0.01\" onchange=\"query()\" id=\"slider-" + index +"\"> <br>";
+        slide_container += `
+                            <label for="slider-${index}">${item}</label>
+                            <input type="range" min="0" max="1" value="0.25" class="slider" step="0.01" onchange="query()" id="slider-${index}">
+                            `;
     });
     document.getElementById("slidercontainer").innerHTML = slide_container;
 }
@@ -95,8 +99,8 @@ function setEnd() {
 }
 
 function get_alpha_vector() {
-    var alpa_vector = [];
-    for (var i = 0; i < metrics.length; i++) {
+    let alpa_vector = [];
+    for (let i = 0; i < metrics.length; i++) {
         alpa_vector.push(parseFloat(document.getElementById("slider-" + i).value));
     }
     sum_alphas = alpa_vector.reduce((a, b) => a + b, 0);
@@ -126,13 +130,13 @@ function query() {
         return;
     }
 
-    var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open("POST", window.location.href + "dijkstra", true);
     xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            var json = JSON.parse(xhr.responseText);
+            let json = JSON.parse(xhr.responseText);
             if (json.path != "") {
                 printPath(json);
                 show_result(json.features[0].properties.cost);
@@ -144,7 +148,7 @@ function query() {
         }
     };
 
-    var body = {
+    const body = {
         "type": "FeatureCollection",
         "features": [
             {
@@ -175,7 +179,7 @@ function query() {
             }
         ]
     };
-    var data = JSON.stringify(body);
+    let data = JSON.stringify(body);
     // console.log("request: " + data);
     xhr.send(data);
 }
@@ -192,7 +196,7 @@ function show_invalid_request() {
 }
 
 function hide_invalid_request() {
-    var x = document.getElementById("invalid-request");
+    let x = document.getElementById("invalid-request");
     if (x.style.display === "block") {
         x.style.display = "none";
     }
@@ -203,7 +207,7 @@ function show_no_path_found() {
 }
 
 function hide_no_path_found() {
-    var x = document.getElementById("no-path-found");
+    let x = document.getElementById("no-path-found");
     if (x.style.display === "block") {
         x.style.display = "none";
     }
@@ -214,26 +218,26 @@ function show_select_start_and_end() {
 }
 
 function hide_select_start_and_end() {
-    var x = document.getElementById("select-start-and-end");
+    let x = document.getElementById("select-start-and-end");
     if (x.style.display === "block") {
         x.style.display = "none";
     }
 }
 
 function show_result(costs) {
-    var tmp = document.getElementById("result")
+    let tmp = document.getElementById("result")
     tmp.innerHTML = "costs: " + costs;
     tmp.style.display = "block";
 }
 
 function hide_result() {
-    var x = document.getElementById("result");
+    let x = document.getElementById("result");
     if (x.style.display === "block") {
         x.style.display = "none";
     }
 }
 
-var greenIcon = new L.Icon({
+const greenIcon = new L.Icon({
     iconUrl: 'img/marker-green.png',
     shadowUrl: 'img/marker-shadow.png',
     iconSize: [25, 41],
@@ -241,7 +245,7 @@ var greenIcon = new L.Icon({
     popupAnchor: [1, -34],
     shadowSize: [41, 41]
 });
-var redIcon = new L.Icon({
+const redIcon = new L.Icon({
     iconUrl: 'img/marker-red.png',
     shadowUrl: 'img/marker-shadow.png',
     iconSize: [25, 41],
