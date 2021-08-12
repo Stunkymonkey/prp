3dd3d77660792a4bf29494c7040e07eab419a1fc
Felix Buehler <account@buehler.rocks>
2021-08-12 16:41:26 +0200

    [analysis/alpha] have the option to select nodes based on location

--- analysis/generate-multiple-alphas-file.py
+++ analysis/generate-multiple-alphas-file.py
@@ -5,15 +5,35 @@ import csv
 import json
 import numpy as np
 import itertools
+from math import radians, cos, sin, asin, sqrt
 
 
 parser = argparse.ArgumentParser(description="generate eval-json")
 parser.add_argument("-i", "--input", type=str, help="input fmi-file", required=True)
 parser.add_argument("-o", "--output", type=str, help="output json-file", required=True)
-parser.add_argument("-s", "--start", type=int, help="starting_id", required=True)
-parser.add_argument("-e", "--end", type=int, help="ending_id", required=True)
+parser.add_argument("-st", "--start-lat", type=float, help="start latitude", required=True)
+parser.add_argument("-sn", "--start-lng", type=float, help="start longitude", required=True)
+parser.add_argument("-et", "--end-lat", type=float, help="end latitude", required=True)
+parser.add_argument("-en", "--end-lng", type=float, help="end longitude", required=True)
 parser.add_argument("-w", "--walk", type=float, help="step size", required=True)
 args = parser.parse_args()
+
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371  # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
 
 
 def convert(o):
@@ -25,7 +45,7 @@ def convert(o):
         raise TypeError
 
 
-def main(input, output, start, end, walk):
+def main(input, output, start_lat, start_lon, end_lat, end_lon, walk):
     nodes = list()
     # read files
     with open(input) as csvfile:
@@ -39,6 +59,18 @@ def main(input, output, start, end, walk):
         for i in range(amount_nodes_fmi):
             tmp = next(fmireader)
             nodes.append({"latitude": float(tmp[2]), "longitude": float(tmp[3])})
+    # find closes points
+    start, end = 0, 0
+    start_dist, end_dist = 999_999, 999_999
+    for index, node in enumerate(nodes):
+        s_dist = haversine(start_lon, start_lat, node["longitude"], node["latitude"])
+        if s_dist < start_dist:
+            start = index
+            start_dist = s_dist
+        e_dist = haversine(end_lon, end_lat, node["longitude"], node["latitude"])
+        if e_dist < end_dist:
+            end = index
+            end_dist = e_dist
     # generate range of the alphas
     alpha_range = np.arange(0.0, 1.0000000000001, walk)
     # generating all possible combinations
@@ -56,4 +88,4 @@ def main(input, output, start, end, walk):
 
 
 if __name__ == '__main__':
-    main(args.input, args.output, args.start, args.end, args.walk)
+    main(args.input, args.output, args.start_lat, args.start_lng, args.end_lat, args.end_lng, args.walk)
